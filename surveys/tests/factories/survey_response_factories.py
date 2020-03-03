23d49ac5834b035199c92ea3c7cd642a36ec7aa2
Jessica Été <kohrVid@zoho.com>
2020-03-03 20:25:26 +0000

    Remove PK field from factories
    Record IDs should be generated by the database and manually adding these
    at the application layer seems to caused more problems in testing that
    it solves.

--- surveys/tests/factories/survey_response_factories.py
+++ surveys/tests/factories/survey_response_factories.py
@@ -3,7 +3,5 @@ from factory.django import DjangoModelFactory
 from faker import Faker
 
 class SurveyResponseFactory(DjangoModelFactory):
-    pk = 1
-
     class Meta:
         model = SurveyResponse
